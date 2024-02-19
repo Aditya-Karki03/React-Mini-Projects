1168be6c9b6d5ffe9d1e5a63f164db8a9aa4a36b
Aditya-Karki03 <adityakarki201@gmail.com>
2024-02-19 22:32:42 +0530

    Star Rater

---
+++ star-rating-comp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Rater from './Components/Rater'
+function App() {
+  return (
+    <div className='App'>
+            <Rater noOfStars={10}/>
+    </div>
+  )
+}
+
+export default App
---
+++ star-rating-comp/src/Components/Rater.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import 'boxicons';
+import './style.css'
+ 
+
+export default function Rater({noOfStars=5}){
+    const[ind,setInd]=React.useState(0);
+    const handleClick=(index)=>{
+
+        setInd(index+1);
+    }
+    return(
+        <div className="main-container">
+            <h1>Star rating component</h1>
+            {
+                [...Array(noOfStars)].map((_,index)=>{
+                    
+                    // index+=1;
+                    return(
+                        <box-icon
+                         type='solid'
+                         name='star'
+                         onClick={()=>handleClick(index)}
+                         color={ind>index?'#fff700':'#000'}
+                         />
+                    )
+                    
+                })
+            }
+        </div>
+    )
+}
